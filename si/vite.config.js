0859f290072d39716624a0fa12e3dc95124fff47
memo9t <129997472+memo9t@users.noreply.github.com>
2025-05-20 19:45:00 -0400

    Update vite.config.js

--- si/vite.config.js
+++ si/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: '0.0.0.0',
+  host: '0.0.0.0',  
+    port: 5173,
+   strictPort: true, 
+    hmr: {
+     protocol: 'ws'  
+    }
   }
-})
+});
